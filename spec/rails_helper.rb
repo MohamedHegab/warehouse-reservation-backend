d0e5a2b45537b3f8775769cb96c8d8b5fec18ae7
Hegab <mohamed@factorial.co>
2022-09-21 16:41:59 +0200

    First commit

---
+++ spec/rails_helper.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+require 'rspec/rails'
+require 'database_cleaner'
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit(1)
+end
+
+DatabaseCleaner.strategy = :truncation
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework(:rspec)
+    with.library(:rails)
+  end
+end
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.include(FactoryBot::Syntax::Methods)
+  config.include(RequestSpecHelper, type: :request)
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
+end
