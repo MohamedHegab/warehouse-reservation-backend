a5b382cf987c4e5edf34d5e59589228d4cac8da4
Hegab <mohamed@factorial.co>
2022-09-22 23:38:27 +0200

    Enable pgcrypto extension so we can add uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_173955) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_213618) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "business_hours", force: :cascade do |t|
