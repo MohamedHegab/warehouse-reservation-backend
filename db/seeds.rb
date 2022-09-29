3fc9b56e33e76cd8469a3e7585a18d104c1b35a2
Hegab <mohamed@factorial.co>
2022-09-29 11:47:01 +0200

    Seed file enhance

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+first_warehouse = Warehouse.find_or_create_by(name: 'Warehouse 1')
+second_warehouse = Warehouse.find_or_create_by(name: 'Warehouse 2')
+third_warehouse = Warehouse.find_or_create_by(name: 'Warehouse 3')
+
+for day in 0..4 do
+  BusinessHour.find_or_create_by(warehouse: first_warehouse, day:, open_time: '08:00', close_time: '21:00')
+end
+
+for day in 0..5 do
+  BusinessHour.find_or_create_by(warehouse: second_warehouse, day:, open_time: '00:00', close_time: '22:00')
+end
+
+for day in 0..6 do
+  BusinessHour.find_or_create_by(warehouse: third_warehouse, day:, open_time: '10:00', close_time: '24:00')
+end
+
+ReservedSlot.find_or_create_by(warehouse: first_warehouse, reservation_name: 'Goods', start_time: '2022-09-29T18:00:00.000Z',
+                               end_time: '2022-09-29T20:30:00.000Z')
+ReservedSlot.find_or_create_by(warehouse: first_warehouse, reservation_name: 'Goods', start_time: '2022-09-28T10:00:00.000Z',
+                               end_time: '2022-09-28T12:30:00.000Z')
+ReservedSlot.find_or_create_by(warehouse: first_warehouse, reservation_name: 'Goods', start_time: '2022-09-29T08:00:00.000Z',
+                               end_time: '2022-09-29T12:00:00.000Z')
